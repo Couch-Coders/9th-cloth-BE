7b1f844c20e49e5d2a267b118f16013f5a08d0ab
lnJoy <lnjoy3306@gmail.com>
2022-07-12 14:38:03 +0900

    feat: return 타입 지정

--- src/addresses/addresses.controller.ts
+++ src/addresses/addresses.controller.ts
@@ -2,33 +2,34 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AddressesService } from './addresses.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
+import { Address } from './entities/address.entity';
 
 @Controller('addresses')
 export class AddressesController {
   constructor(private readonly addressesService: AddressesService) {}
 
   @Post()
-  create(@Body() createAddressDto: CreateAddressDto) {
+  async create(@Body() createAddressDto: CreateAddressDto): Promise<Address> {
     return this.addressesService.create(createAddressDto)
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Address[]> {
     return this.addressesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id') id: number): Promise<Address> {
     return this.addressesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: number, @Body() updateAddressDto: UpdateAddressDto) {
+  update(@Param('id') id: number, @Body() updateAddressDto: UpdateAddressDto): Promise<Address> {
     return this.addressesService.update(id, updateAddressDto);
   }
 
   @Delete(':id')
-  softDelete(@Param('id') id: number) {
-    return this.addressesService.softDelete(id);
+  async softDelete(@Param('id') id: number): Promise<void> {
+    this.addressesService.softDelete(id);
   }
 }
