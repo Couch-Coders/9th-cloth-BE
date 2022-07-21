930e16d8dbc6472223bae6e20907ff3eca6375fd
lnJoy <20sunrin007@sunrint.hs.kr>
2022-07-21 20:33:14 +0900

    feat: address 데이터 관리하는 함수 추가

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -4,10 +4,14 @@ import { StoresController } from './stores.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Store } from './entities/store.entity';
 import { UsersModule } from 'src/users/users.module';
+import { AddressesModule } from 'src/addresses/addresses.module';
+import { APP_GUARD } from '@nestjs/core';
+import { SellerGuard } from 'src/seller/seller.guard';
 
 @Module({
   imports: [
     UsersModule,
+    AddressesModule,
     TypeOrmModule.forFeature([Store]),    
   ],
   controllers: [StoresController],
