44c3436f7f4892dad852a503c61f311133248f3f
lnJoy <20sunrin007@sunrint.hs.kr>
2022-07-26 16:57:48 +0900

    fix: SellerGuard가 없어졌기 때문에 UsersMoudle 또한 삭제제

--- src/clothes/clothes.module.ts
+++ src/clothes/clothes.module.ts
@@ -3,13 +3,9 @@ import { ClothesService } from './clothes.service';
 import { ClothesController } from './clothes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cloth } from './entities/cloth.entity';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule.forFeature([Cloth]),
-  ],
+  imports: [TypeOrmModule.forFeature([Cloth])],
   controllers: [ClothesController],
   providers: [ClothesService],
   exports: [ClothesService],
